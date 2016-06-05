242c76cd44960569d8c457928db75c37d781951d
Andrew Donnellan <andrew@donnellan.id.au>
2016-06-05 16:11:43 +1000

    Initial commit of KernelPRBot script and documentation
    Signed-off-by: Andrew Donnellan <andrew@donnellan.id.au>

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # KernelPRBot
+
+# Installation
+
+Install dependencies:
+
+    pip3 install -r requirements.txt
+
+Set up configuration in settings.py:
+
+	GITHUB_TOKEN="<token here>"
+	MESSAGE_PATH="/path/to/message.md"
+	STATUS_FILE="/path/to/status.json"
+	REPO_NAME="username/repo"
+
+Add a job to your crontab, e.g.:
+
+	*/5 * * * * /home/username/KernelPRBot/kernelprbot.py 2>&1 | /usr/bin/logger
+
+This example will run the script every 5 minutes and send all output
+to syslog.
+
+# Copyright
+
+Copyright &copy; 2016 Andrew Donnellan.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ kernelprbot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# KernelPRBot - monitor a GitHub repository for new pull requests and post a comment
+# Copyright (C) 2016 Andrew Donnellan <andrew@donnellan.id.au>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import github
+import jinja2
+from settings import GITHUB_TOKEN, MESSAGE_PATH, STATUS_FILE, REPO_NAME
+
+def poll(repo, msg, status):
+    pulls = repo.get_pulls(sort='created')
+    for pull in pulls.reversed:
+        print("Pull request #{} by @{}: {}".format(pull.number, pull.user.login, pull.title))
+        if pull.number <= status['pull_req_number']:
+            print(" => Lower than threshold ({}), breaking".format(status['pull_req_number']))
+            break
+        comment = msg.render(username=pull.user.login)
+        try:
+            pull.create_issue_comment(comment)
+            status['pull_req_number'] = max(status['pull_req_number'], pull.number)
+            print(" => Comment posted successfully")
+        except:
+            print(" => Error occurred when posting comment")
+
+def main():
+    try:
+        status = json.load(open(STATUS_FILE))
+    except FileNotFoundError:
+        status = {'pull_req_number': 0}
+
+    msg = jinja2.Template(open(MESSAGE_PATH).read())
+    gh = github.MainClass.Github(GITHUB_TOKEN)
+    repo = gh.get_repo(REPO_NAME)
+
+    poll(repo, msg, status)
+    json.dump(status, open(STATUS_FILE, 'w'))
+
+if __name__ == '__main__':
+    main()
